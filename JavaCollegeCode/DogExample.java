f75ebbef986ff134797528e9c2e51efce2cfe1d5
Hal Rymel <157781904+RymelE@users.noreply.github.com>
2025-10-05 19:17:49 -0500

    Add files via upload
    Same thing this is for learning objects in java from course work

---
+++ JavaCollegeCode/DogExample.java
@@ -0,0 +1,50 @@
+/**
+ * DogExample contains a main method to test the Dog class constructors and methods
+ */
+public class DogExample {
+	public static void main(String[] args) {
+		
+		//TODO: Create a third dog named Woofster who is wagging their tail and like baths
+        Dog dog1 = new Dog("Snuggles", true, false); 
+        Dog dog2 = new Dog("Fido", false, true);
+        
+        //TODO: Print initial state of all 3 dogs
+        System.out.println("After creating new dogs:");
+        dog1.printState();
+        dog2.printState();
+        
+        //TODO: Give all 3 dogs a bath
+       
+        
+        //TODO: Print final state of all 3 dogs
+        System.out.println("After baths:");
+        dog1.printState();
+        dog2.printState();
+  
+    }
+}
+
+/**
+ * Dog class demonstrates fields, constructors, and methods
+ */
+class Dog {
+
+    // Instance variables/fields
+     private String name;   
+     private boolean isWaggingTail;
+     private boolean likesBaths;
+     
+     //Constructor
+     public Dog(String newName, boolean newIsWaggingTail, boolean newLikesBaths) {
+         name = newName;
+         isWaggingTail = newIsWaggingTail;
+         likesBaths = newLikesBaths;
+     }
+     
+     public void printState() {
+    	 System.out.println(name + " is wagging: " + isWaggingTail + ", likes baths: " + likesBaths);
+     }
+     
+     //TODO: Implement giveBath method to assign isWaggingTail to likesBaths
+     
+}
