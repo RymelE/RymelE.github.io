bd150cc6d858b156720c4a443ba112fa16ada69b
Hal Rymel <157781904+RymelE@users.noreply.github.com>
2025-10-10 08:40:37 -0500

    Update main.py
    add app functionality

--- Pokedex_Python_Tkinter/main.py
+++ Pokedex_Python_Tkinter/main.py
@@ -1,21 +1,23 @@
 import tkinter as tk
 import json 
 
-
 root = tk.Tk()
+root.geometry("800x800")
+
+
 #button command to call the id number from the json file
 def button_command(pokemon_id):
-    print("Whos that Pokemon")
-    print({pokemon_id})
+    pokemon_id = int(entry.get())
     try:
         with open('pokemon.json', 'r', encoding='utf-8') as file:
             data = json.load(file)  # data is a list of dicts
 
         for pokemon in data:
             if pokemon['id'] == pokemon_id:
-                print("id:", pokemon['id'])
-                print("name:", pokemon['name']['english'])
-                print("type:", pokemon['type'])
+                name_label.config(text=f"Name: {pokemon['name']['english']}")
+                id_label.config(text=f"ID: {pokemon['id']}")
+                type_label.config(text=f"Type: {pokemon['type']}")
+
                 break
         else:
             print(f"Error: no Pokémon with that Pokédex number {pokemon_id}")
@@ -30,11 +32,20 @@ def button_command(pokemon_id):
 
 root.title("Pokemon")
 
-label = tk.Label(root, text="Pokémon Pokedex", font=("Arial", 14))
-label.pack(padx=20, pady=20)
+name_label = tk.Label(root, text="Name")
+name_label.grid(column=2, row=2)
+id_label = tk.Label(root, text="ID")
+id_label.grid(column=3,row=4)
+type_label = tk.Label(root, text="Type")
+type_label.grid(column=3,row=5)
+title1_label = tk.Label(root, text="Pokémon Pokedex", font=("Arial", 14))
+title1_label.grid(column=1, row=0, padx=20, pady=20)
 
-# using 1 for a test
+entry = tk.Entry()
+entry.grid(column=0, row=2, pady=5)
+
 button = tk.Button(root, text="Who's that Pokémon?", command=lambda: button_command(1))
-button.pack(pady=10)
-#to do add blank to type a number for the button to call on
+button.grid(column=1,row=2, pady=10)
+
+
 root.mainloop()
