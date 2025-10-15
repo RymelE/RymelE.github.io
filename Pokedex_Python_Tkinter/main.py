d59e53f089399d234c6b79a777e3b0681f66aa0e
Hal Rymel <157781904+RymelE@users.noreply.github.com>
2025-10-14 21:21:27 -0500

    Update main.py
    added functionality for stats

--- Pokedex_Python_Tkinter/main.py
+++ Pokedex_Python_Tkinter/main.py
@@ -4,7 +4,7 @@ import json
 root = tk.Tk()
 root.geometry("800x800")
 
-
+#url = 
 #button command to call the id number from the json file
 def button_command(pokemon_id):
     pokemon_id = int(entry.get())
@@ -15,9 +15,14 @@ def button_command(pokemon_id):
         for pokemon in data:
             if pokemon['id'] == pokemon_id:
                 name_label.config(text=f"Name: {pokemon['name']['english']}")
-                id_label.config(text=f"ID: {pokemon['id']}")
+                
                 type_label.config(text=f"Type: {pokemon['type']}")
-
+                hp.config(text=f"HP: {pokemon['base']['HP']}")
+                Att.config(text=f"HP: {pokemon['base']['Attack']}")
+                Def.config(text=f"HP: {pokemon['base']['Defense']}")
+                Spa_Att.config(text=f"HP: {pokemon['base']['Sp. Attack']}")
+                Spa_Def.config(text=f"HP: {pokemon['base']['Sp. Defense']}")
+                Speed.config(text=f"HP: {pokemon['base']['Speed']}")
                 break
         else:
             print(f"Error: no Pokémon with that Pokédex number {pokemon_id}")
@@ -28,24 +33,44 @@ def button_command(pokemon_id):
         print("Error: JSON file is not valid.")
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
-
-
+#title
 root.title("Pokemon")
 
-name_label = tk.Label(root, text="Name")
-name_label.grid(column=2, row=2)
-id_label = tk.Label(root, text="ID")
-id_label.grid(column=3,row=4)
-type_label = tk.Label(root, text="Type")
-type_label.grid(column=3,row=5)
+#labels pokedex
 title1_label = tk.Label(root, text="Pokémon Pokedex", font=("Arial", 14))
-title1_label.grid(column=1, row=0, padx=20, pady=20)
+title1_label.grid(column=2, row=0, padx=20, pady=20)
+
+#picture
+#pic
+
+name_label = tk.Label(root, text="Name",font=("Arial", 14))
+name_label.grid(column=3, row=3)
+
+
+type_label = tk.Label(root, text="Type",font=("Arial", 14))
+type_label.grid(column=3,row=4)
+
+#stats label
+frame = tk.Frame(root)
+frame.grid(row=5,column=0)
+hp = tk.Label(frame, text="HP",font=("Arial", 14))
+hp.grid(column=1,row=0)
+Att = tk.Label(frame, text="Att",font=("Arial", 14))
+Att.grid(column=2, row=0)
+Def = tk.Label(frame, text="Def",font=("Arial", 14))
+Def.grid(column=3, row=0)
+Spa_Att = tk.Label(frame, text="Spa Att",font=("Arial", 14))
+Spa_Att.grid(column=4, row=0)
+Spa_Def = tk.Label(frame, text="Spa Deff",font=("Arial", 14))
+Spa_Def.grid(column=5, row=0)
+Speed = tk.Label(frame, text="Speed",font=("Arial", 14))
+Speed.grid(column=6,row=0)
+
 
 entry = tk.Entry()
-entry.grid(column=0, row=2, pady=5)
+entry.grid(column=3, row=2, pady=5)
 
 button = tk.Button(root, text="Who's that Pokémon?", command=lambda: button_command(1))
 button.grid(column=1,row=2, pady=10)
-
 
 root.mainloop()
